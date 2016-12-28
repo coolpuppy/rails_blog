6f5afe3417be3c6d4908bf716a8b90cfdbcf28cb
coolpuppy <marvelouspuppy@gmail.com>
2016-12-28 01:47:50 -0800

    improve user create process and check user logged_in in some web page. add model relationship, finishing user following and follower function.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id])
   end
+
+  def logged_in_user
+    unless session[:user_id]
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ApplicationRecord
   before_save :encrypt_password
+  before_save :downcase_email
   after_save :clear_password
 
   has_many :blogs, dependent: :destroy
+  has_many :active_relationships,  class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent: :destroy
+
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :password
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -11,11 +21,16 @@ class User < ApplicationRecord
   validates :password, :confirmation => true
   validates_length_of :password, :in => 6..20, :on => :create
 
-  def encrypt_password
-    if password.present?
-      self.salt = BCrypt::Engine.generate_salt
-      self.encrypted_password= BCrypt::Engine.hash_secret(password, salt)
-    end
+  def follow(other_user)
+    active_relationships.create(follower_id: self.id, followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.destroy(follower_id: self.id, followed_id: other_user.id)
+  end
+
+  def following?(other_user)
+    followings.include?(other_user)
   end
 
   def clear_password
@@ -29,4 +44,16 @@ class User < ApplicationRecord
   def gen_token
     Digest::SHA1.hexdigest(self.email + self.salt)
   end
+
+  private
+    def encrypt_password
+      if password.present?
+        self.salt = BCrypt::Engine.generate_salt
+        self.encrypted_password= BCrypt::Engine.hash_secret(password, salt)
+      end
+    end
+
+    def downcase_email
+      self.email = email.downcase
+    end
 end
